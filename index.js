20a8284335095129a141722f7895c14ab54a1d17
SinxLinonZ <urara.kbys@gmail.com>
2022-06-02 14:14:37 +0900

    Fix: mongodb connection url

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const wsServer = new ws.Server({ noServer: true });
 
 // Database initialization
 const { MongoClient } = require('mongodb');
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb://127.0.0.1:27017';
 const dbName = 'test_msg';
 const client = new MongoClient(url);
 let db;
